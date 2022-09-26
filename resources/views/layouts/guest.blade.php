5063b1973f8c423970f6e0127ba8eb2f26c7edb2
Juliet Linarez <julietlinarez@gmail.com>
2022-09-26 12:17:59 +0200

    working in home page

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -22,3 +22,4 @@
         </div>
     </body>
 </html>
+ --}}
