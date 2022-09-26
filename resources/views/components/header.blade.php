c54a41eaab6829b4d1e5dcbd5bb3d0655881df64
Juliet Linarez <julietlinarez@gmail.com>
2022-09-26 12:45:49 +0200

    component head and header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,6 +1,4 @@
-<header>
-    <div>
-    {{-- <img src="{{ asset('./img/logofem.png') }}" alt=""> --}}
+<header class="header">
+    <img src="{{ $image->url_file }}" alt="">
     <h1>HOLA</h1>
-    </div>
 </header>
