68c7264bf5f41ffc15359701788d3adf03c78fc2
Sara <sarap120298@gmail.com>
2022-09-27 10:51:08 +0200

    merge

--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,7 @@ Route::delete('/admin/workshops', [WorkshopController::class, 'destroy'])->name(
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+})->middleware(['auth','verified'])->name('dashboard');
 
 require __DIR__.'/auth.php';
 
