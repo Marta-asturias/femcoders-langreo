e681148b3324166fb2f38caf020ade3c62983add
unknown <julietlinarez@gmail.com>
2022-09-27 12:49:30 +0200

    home

--- resources/views/components/seccionclain.blade.php
+++ resources/views/components/seccionclain.blade.php
@@ -1,5 +1,27 @@
-<div>
-<img src="{{ asset('img/chicas/foto5.jpeg') }}" />
+<div class="conten-clain">
+
+<div class="img-principal">
+<img src="{{ asset('img/foto1.jpeg') }}" alt="">
+</div>
+
+<div class="text-clain">
+¿Conoces a Ada Lovelace o Grae Hopper? Son sólo dos ejemplo de las numerosas mujeres que revolucionaron el mundo de la tecnología. Es hora de volver a cambiar la historia y romper los estereotipos, porque... ¡Todas podemos aprender a programar¡
+Tú también puedes apender a crear tu propia app, juegos o páginas web.
+¡Es mucho más facil y divertido de lo que te imaginas!
+Atrévete a descubrir cómo funciona el código, y comprueba por ti misma que la tecnología ¡mola! 
+
+</div>
+
+<div class="img-logo">
+    <div class="logo_clain"><img src="" alt=""></div>
+    <div class="logo_clain"><img src="" alt=""></div>
+    <div class="logo_clain"><img src="" alt=""></div>
+</div>
+
+<div class="btn-clain">
+    <a href="{{ url('/register') }}" class="btn-clain">Apuntate Gratis</a>
+</div>
+
 </div>
 
 
