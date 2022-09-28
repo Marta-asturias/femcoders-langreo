cecbdd1506c07bb834183e9f2f95e2e66580d125
Juliet Linarez <julietlinarez@gmail.com>
2022-09-28 09:42:30 +0200

    change clain text style

--- resources/views/components/mini-game.blade.php
+++ resources/views/components/mini-game.blade.php
@@ -2,9 +2,12 @@
 <div class="title-mg">
     <h3 class="txt-mg">Mini Juegos</h3>
 </div>
-<div></div>
-<div></div>
-
-
-
+<div>
+        <img src="..." class="card-mj" alt="...">
+        <h5 class="mjtitle">Juego</h5>
+        <a href="#" class="btn-mj">Jugar</a>
+</div>
+<div class="btn-mj2">
+    <a href="{{ url('/') }}" class="btn-b">Ver Más</a>
+</div>
 </section>
--- resources/views/components/seccionclain.blade.php
+++ resources/views/components/seccionclain.blade.php
@@ -6,11 +6,12 @@
 
 <div class="text-clain">
 <p class="txt-clain">
+    <strong>
 ¿Conoces a Ada Lovelace o Grae Hopper? Son sólo dos ejemplo de las numerosas mujeres que revolucionaron el mundo de la tecnología. Es hora de volver a cambiar la historia y romper los estereotipos, porque... ¡Todas podemos aprender a programar¡
 Tú también puedes apender a crear tu propia app, juegos o páginas web.
 ¡Es mucho más facil y divertido de lo que te imaginas!
 Atrévete a descubrir cómo funciona el código, y comprueba por ti misma que la tecnología ¡mola! 
-</p>
+</strong></p>
 </div>
 
 <div class="img-logo">
@@ -28,7 +29,7 @@ Atrévete a descubrir cómo funciona el código, y comprueba por ti misma que la
 </div>
 
 <div class="btn-clain">
-    <a href="{{ url('/register') }}" class="btn-A">Apuntate Gratis</a>
+    <a href="{{ url('/') }}" class="btn-A">Apuntate Gratis</a>
 </div>
 
 </section>
