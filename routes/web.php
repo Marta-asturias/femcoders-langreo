30bd8ea55c4a41de755bc9f8cd5d493b2b3e4b78
Sara <sarap120298@gmail.com>
2022-09-28 13:31:59 +0200

    email created

--- routes/web.php
+++ routes/web.php
@@ -40,7 +40,9 @@ Route::delete('/admin/workshops/workshops', [WorkshopController::class, 'destroy
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth','verified'])->name('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
 
+// Email related routes
+Route::get('mail/send', 'MailController@send');
