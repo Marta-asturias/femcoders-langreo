11f8e84c31ed16091772b8664b90a9a91b27a0f8
Juliet Linarez <julietlinarez@gmail.com>
2022-09-29 11:53:22 +0200

    check section courses in home

--- resources/views/components/seccion-courses.blade.php
+++ resources/views/components/seccion-courses.blade.php
@@ -1,3 +1,83 @@
-<div>
-    <!-- He who is contented is rich. - Laozi -->
+<div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+    <div class="carousel-item active">
+        <div class="section-courses">
+            <div class="conten-courses">
+                <div class="img-courses">
+                    <img src="{{ asset('img/foto7.jpg') }}" />
+                </div>
+                <div class="conten2-courses">
+                    <div class="title-courses">
+                        <h4>Título del Curso</h4>
+                    </div>
+                    <div class="separador"></div>
+                    <div class="text-course">
+                        <p>HOLA</p>
+                    </div>
+                </div>
+                <div class="btn-cuorses"> 
+                <a href="">
+                <button type="button" class="C-btn">Inscribete</button> 
+                </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="carousel-item">
+        <div class="section-courses">
+            <div class="conten-courses">
+                <div class="img-courses">
+                    <img src="{{ asset('img/foto8.jpg') }}" />
+                </div>
+                <div class="conten2-courses">
+                    <div class="title-courses">
+                        <h4>Título del Curso</h4>
+                    </div>
+                    <div class="separador"></div>
+                    <div class="text-course">
+                        <p>HOLA</p>
+                    </div>
+                </div>
+                <div class="btn-cuorses"> 
+                <a href="">
+                <button type="button" class="C-btn">Inscribete</button> 
+                </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="carousel-item">
+        <div class="section-courses">
+            <div class="conten-courses">
+                <div class="img-courses">
+                    <img src="{{ asset('img/foto9.jpg') }}" />
+                </div>
+                <div class="conten2-courses">
+                    <div class="title-courses">
+                        <h4>Título del Curso</h4>
+                    </div>
+                    <div class="separador"></div>
+                    <div class="text-course">
+                        <p>HOLA</p>
+                    </div>
+                </div>
+                <div class="btn-cuorses"> 
+                <a href="">
+                <button type="button" class="C-btn">Inscribete</button> 
+                </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
 </div>
+
+<div class="btn2-cuorses"> 
+    <a href="">
+    <button type="button" class="C2-btn" >Ver Más</button> 
+    </a>
+</div>
+
+
+
+            
