e63c9a07cc0257528ee3f1b706d2dd78051fb64e
Juliet Linarez <julietlinarez@gmail.com>
2022-09-30 12:40:33 +0200

    check image and text principal view workshop

--- resources/views/minigames.blade.php
+++ resources/views/minigames.blade.php
@@ -1,10 +1,10 @@
 <x-head/> 
 <x-header/>
-<div class="txt-principal-MG">
-    <h1 class="title-principal-MG">Mini Juegos</h1>
+<div class="title-principal">
+    <h1 class="txt-principal">Mini Juegos</h1>
 </div>
 
-<div class="img-principal-MG">
+<div class="img-principal">
     <img src="{{ asset('img/foto9.jpg') }}" alt="">
 </div>
 <x-miniGameCard :minigame="$minigame"/>
