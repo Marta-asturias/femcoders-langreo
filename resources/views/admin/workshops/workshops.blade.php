b4111ec669aefd93d2b8b06087a308379c5427c8
Juliet Linarez <julietlinarez@gmail.com>
2022-10-03 12:42:55 +0200

    working in the admin session

--- resources/views/admin/workshops/workshops.blade.php
+++ resources/views/admin/workshops/workshops.blade.php
@@ -6,24 +6,23 @@
 </div>
 
 <button class="AdminH2">
-    <a href="{{ url('/') }}" class="btnH2">Apuntate Gratis</a>
+    <a href="{{ url('/admin/workshops/create') }}" class="btnH2">Agregar Nuevo</a>
 </button>
 
 @foreach($workshop as $workshops)
-<table class="table-W">
-    <tr class="table-W1">
-    <td class="table-W2">
-        <h2>{{$workshops->title}}</h2>
-    </td>
-    </tr>
-</table>
+    <div class="table-W">
+        <div class="lineasW">
+            <h2>{{$workshops->title}}</h2>
+            <form   class="AdminB2" action="{{ route('destroy',['id'=>$workshops->id]) }}" method="POST">
+                <button type="submit" class="btnB2">Borrar</button>
+                @csrf
+                @method('DELETE')
+            </form>
+            @endforeach
+        </div>
+    </div>
 
-<form   class="AdminB2" action="{{ route('destroy',['id'=>$workshops->id]) }}" method="POST">
-    <button type="submit" class="btnB2">Borrar</button>
-    @csrf
-    @method('DELETE')
-</form>
-@endforeach
+
 
 
 <x-footer/>
