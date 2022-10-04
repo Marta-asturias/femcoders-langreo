04fda7d49c525d32195b2aff5b5609ea7337419f
Marta-asturias <marta.gonzalez@domoticadavinci.com>
2022-10-04 12:03:30 +0200

    admin create errors

--- resources/views/admin/workshops/create.blade.php
+++ resources/views/admin/workshops/create.blade.php
@@ -1,4 +1,5 @@
 <x-head />
+
 <h1 class="workshopscreate">Añadir Talleres</h1>
 
 <div class="conten-create">
@@ -11,53 +12,53 @@
             </ul>
         </div>
     @endif
+
     <form action="{{ route('admin.save') }}" method="POST" enctype="multipart/form-data">
         @csrf
 
         <div class="tile-create">
-            <x-label class="text-title-c" for="title" :value="__('Añadir Titulo')" />
-            <x-input id="title" class="input-tilte-create" type="text" name="title" :value="old('title')" required
+            <x-label for="title" :value="__('Añadir Titulo')" />
+            <x-input id="title" type="text" name="title" :value="old('title')" required
                 autofocus />
         </div>
 
 
 
 
-        <div class="create-1">
-            <input type="file" name="file" class="btn-img-c">
+{{--         <div class="create-1">
+            <input type="file" name="file">
         </div>
-
-        <div class="create-2">
-            <div class="text-des-create text-des-create2">
-                <x-label for="price" :value="__('')" />
-                <span>Detalles:</span>
-                <x-input id="details" type="text" name="details" :value="old('details')" required />
+ --}}
+       
+            <div class="create-2">
+                <x-label class="label" for="price" :value="__('detalles')" />
+                <x-input class="create" type="text" name="details" :value="old('details')" required />
             </div>
 
-            <div class="text-des-create">
+            <div class="create-3">
                 <x-label for="price" :value="__('')" />
                 <span>Edad:</span>
                 <x-input id="age" type="text" name="age" :value="old('age')" required />
             </div>
 
-            <div class="text-des-create">
+            <div class="create-4">
                 <x-label for="price" :value="__('')" />
                 <span>Duración:</span>
                 <x-input id="duration" type="text" name="duration" :value="old('duration')" required />
             </div>
 
-            <div class="text-des-create">
+            <div class="create-5">
                 <x-label for="price" :value="__('')" />
                 <span>Formato:</span>
                 <x-input id="format" type="text" name="format" :value="old('format')" required />
             </div>
         </div>
-</div>
 
-<div class="btn-create">
-    <button class="btn-c">
-        {{ __('Guardar') }}
-    </button>
-</div>
-</form>
+
+    <div class="btn-create">
+        <button class="btn-c">
+            {{ __('Guardar') }}
+        </button>
+    </div>
+    </form>
 </div>
