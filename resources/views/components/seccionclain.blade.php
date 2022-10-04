15962eacc8008b445c0fc78a489c73983834238b
Paz Manrique <nautilus1985@gmail.com>
2022-10-05 01:11:28 +0200

    playing with the front of claim

--- resources/views/components/seccionclain.blade.php
+++ resources/views/components/seccionclain.blade.php
@@ -1,16 +1,26 @@
 <section class="conten-clain">
 
-    <div class="img-principal">
-    <img src="{{ asset('img/foto3.jpeg') }}" alt="">
-    </div>
+  <div class="img-principal" >
+    <img class="girl" src="{{ asset('img/foto3.jpeg') }}" alt=""> 
+    </div> 
     
     <div class="text-clain">
-    <p class="txt-clain">
-    ¿Conoces a Ada Lovelace o Grae Hopper? Son sólo dos ejemplo de las numerosas mujeres que revolucionaron el mundo de la tecnología. Es hora de volver a cambiar la historia y romper los estereotipos, porque... ¡Todas podemos aprender a programar¡
-    Tú también puedes apender a crear tu propia app, juegos o páginas web.
-    ¡Es mucho más facil y divertido de lo que te imaginas!
-    Atrévete a descubrir cómo funciona el código, y comprueba por ti misma que la tecnología ¡mola! 
-    </p>
+    <p class="txt-clain1">
+        <strong>
+    ¿Conoces a Ada Lovelace o Grae Hopper? Son sólo dos ejemplo de las numerosas mujeres que revolucionaron el mundo de la tecnología. 
+    </strong></p>
+
+    <p class="text-clain2">
+        <strong>
+            Es hora de volver a cambiar la historia y romper los estereotipos, porque... ¡Todas podemos aprender a programar¡</strong></p>
+    
+        <p class="txt-clain3">
+            <strong>
+                
+        Tú también puedes apender a crear tu propia app, juegos o páginas web.
+        ¡Es mucho más facil y divertido de lo que te imaginas!<br>
+        Atrévete a descubrir cómo funciona el código, y comprueba por ti misma que la tecnología ¡mola! 
+        </strong></p>
     </div>
     
     <div class="img-logo">
@@ -27,11 +37,12 @@
         
     </div>
     
-    <div class="btn-clain">
-        <a href="{{ url('/register') }}" class="btn-A">Apuntate Gratis</a>
-    </div>
+    <button class="btn-clain">
+        <a href="{{ url('/') }}" class="btn-A">Apuntate Gratis</a>
+    </button>
     
     </section>
+    
     
     
     
