54c5ac39ad3275f39ad48c8234345c3aa63ee2dc
Juliet Linarez <julietlinarez@gmail.com>
2022-10-04 14:47:38 +0200

    fixe in admin

--- resources/views/admin/workshops/workshops.blade.php
+++ resources/views/admin/workshops/workshops.blade.php
@@ -9,27 +9,23 @@
     <a href="{{ url('/admin/workshops/create') }}" class="btnH2">Agregar Nuevo</a>
 </button>
 
-<div class="conten-P">
 
-            @foreach($workshop as $workshops)
-    <div class="linea">
-            <h2> <strong>{{$workshops->title}}</strong></h2>
-            
-            <button class="btn-Edit">
-                <a href="{{ url('/admin/workshops//edit') }}" class="btn-Edit"><i class="bi bi-pencil"></i> Modificar</a>
-            </button>
 
-            <form   class="AdminB2" action="{{ route('destroy',['id'=>$workshops->id]) }}" method="POST">
-                <button type="submit" class="btn-Delete"><i class="bi bi-trash"></i>Borrar</button>
-                @csrf
-                @method('DELETE')
-            </form>
-            @endforeach
+@foreach($workshop as $workshops)
+    <div class="conten-P">
+        <div class="linea">
+                <h2> <strong>{{$workshops->title}}</strong></h2>
+                <button class="btn-Edit">
+                    <a href="{{ route('edit',['id'=>$workshops->id]) }}" class="btn-Edit"><i class="bi bi-pencil"></i> Modificar</a>
+                </button>
 
-            
+                <form   class="AdminB2" action="{{ route('destroy',['id'=>$workshops->id]) }}" method="POST">
+                    <button class="btn-Delete-A" type="submit"> <i class="bi bi-trash"></i>Borrar</button>
+                    @csrf
+                    @method('DELETE')
+                </form>
+        </div>       
     </div>
-
-</div>
-
+@endforeach
 
 <x-footer/>
