c26f608f8ca12804a14ed9bc1aaec14ee9d7feb2
Paz Manrique <nautilus1985@gmail.com>
2022-10-04 22:35:04 +0200

    seccion claim

--- resources/views/components/seccionclain.blade.php
+++ resources/views/components/seccionclain.blade.php
@@ -1,62 +1,37 @@
-<div class="Sectio-Clain">
+<section class="conten-clain">
 
-    <div class="cardList">
-
-
-        <div class="cards__wrapper">
-            
-            <div class="card current--card">
-                <div class="card__image">
-                    <img src="{{ asset('img/foto3.jpeg') }}" />
-                </div>
-            </div>
-
-            <div class="card next--card">
-                <div class="card__image">
-                    <img src="{{ asset('img/foto6.jpg') }}" />
-                </div>
-            </div>
-
-            <div class="card previous--card">
-                <div class="card__image">
-                    <img src="{{ asset('img/foto4.jpg') }}" />
-                </div>
-            </div>
-        </div>
+    <div class="img-principal">
+    <img src="{{ asset('img/foto3.jpeg') }}" alt="">
+    </div>
+    
+    <div class="text-clain">
+    <p class="txt-clain">
+    ¿Conoces a Ada Lovelace o Grae Hopper? Son sólo dos ejemplo de las numerosas mujeres que revolucionaron el mundo de la tecnología. Es hora de volver a cambiar la historia y romper los estereotipos, porque... ¡Todas podemos aprender a programar¡
+    Tú también puedes apender a crear tu propia app, juegos o páginas web.
+    ¡Es mucho más facil y divertido de lo que te imaginas!
+    Atrévete a descubrir cómo funciona el código, y comprueba por ti misma que la tecnología ¡mola! 
+    </p>
+    </div>
+    
+    <div class="img-logo">
+        <a href="https://www.google.org/">
+            <div class="logo_clain"> <img src="{{ asset('img/google.png') }}" /></div>
+        </a>
+        <a href="https://factoriaf5.org/?gclid=CjwKCAjwvsqZBhAlEiwAqAHEle2poQfyI4jKkghztDXqVYqnudThhLZr5u0iuligplH6hW9QOEVztBoCYygQAvD_BwE">
+            <div class="logo_clain"><img src="{{ asset('img/factoria-web.png') }}" /></div>
+        </a>
+        
+        <a href="https://femcoders.factoriaf5.org/">
+            <div class="logo_clain"><img src="{{ asset('img/logofempng.png') }}" /></div>
+        </a>
         
     </div>
-
     
-
-    <div class="infoList">
-        <div class="info__wrapper">
-            <div class="info current--info">
-                <div class="text">
-                    ¿Conoces a Ada Lovelace o Grae Hopper?  Son sólo dos ejemplo de las numerosas mujeres que revolucionaron el mundo de la tecnología. Es hora de volver a cambiar la historia y romper los estereotipos, porque... ¡Todas podemos aprender a programar¡
-                    Tú también puedes apender a crear tu propia app, juegos o páginas web.
-                    ¡Es mucho más facil y divertido de lo que te imaginas!
-                    Atrévete a descubrir cómo funciona el código, y comprueba por ti misma que la tecnología ¡mola!
-                </div>
-                <button class="btn-clain">
-                    <a href="{{ url('/workshops') }}" class="btn-A">Apuntate Gratis</a>
-                </button>
-
-                <div class="img-logo">
-                    <a href="https://femcoders.factoriaf5.org/">
-                        <div class="logo_clain"><img src="{{ asset('img/logofempng.png') }}" /></div>
-                    </a>
-
-                    <a href="https://www.google.org/">
-                        <div class="logo_clain"> <img src="{{ asset('img/google.png') }}" /></div>
-                    </a>
-                    <a href="https://factoriaf5.org/?gclid=CjwKCAjwvsqZBhAlEiwAqAHEle2poQfyI4jKkghztDXqVYqnudThhLZr5u0iuligplH6hW9QOEVztBoCYygQAvD_BwE">
-                        <div class="logo_clain"><img src="{{ asset('img/factoria-web.png') }}" /></div>
-                    </a>               
-                    
-                    
-                </div>
-            </div>
-        </div>
+    <div class="btn-clain">
+        <a href="{{ url('/register') }}" class="btn-A">Apuntate Gratis</a>
     </div>
     
-</div>
+    </section>
+    
+    
+    
