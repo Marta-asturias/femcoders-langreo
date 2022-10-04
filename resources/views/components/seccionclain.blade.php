bc26b1e9062e1a58e34f4c9f235e854ea42b6931
Juliet Linarez <julietlinarez@gmail.com>
2022-10-04 08:45:18 +0200

    new clain in HTML

--- resources/views/components/seccionclain.blade.php
+++ resources/views/components/seccionclain.blade.php
@@ -1,3 +1,49 @@
+<div class="Sectio-Clain">
+
+    <div class="cardList btn--left">
+
+
+        <div class="cards__wrapper">
+            
+            <div class="card current--card">
+                <div class="card__image">
+                    <img src="{{ asset('img/foto3.jpeg') }}" />
+                </div>
+            </div>
+
+            <div class="card next--card">
+                <div class="card__image">
+                    <img src="{{ asset('img/foto2.jpeg') }}" />
+                </div>
+            </div>
+
+            <div class="card previous--card btn--right">
+                <div class="card__image">
+                    <img src="{{ asset('img/foto8.jpg') }}" />
+                </div>
+            </div>
+        </div>
+        
+    </div>
+
+    
+
+    <div class="infoList">
+        <div class="info__wrapper">
+            <div class="info current--info">
+                <div class="text">
+                    ¿Conoces a Ada Lovelace o Grae Hopper?  Son sólo dos ejemplo de las numerosas mujeres que revolucionaron el mundo de la tecnología. Es hora de volver a cambiar la historia y romper los estereotipos, porque... ¡Todas podemos aprender a programar¡
+                    Tú también puedes apender a crear tu propia app, juegos o páginas web.
+                    ¡Es mucho más facil y divertido de lo que te imaginas!
+                    Atrévete a descubrir cómo funciona el código, y comprueba por ti misma que la tecnología ¡mola!
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+
 {{-- <section class="conten-clain">
 
 <div class="img-principal">
