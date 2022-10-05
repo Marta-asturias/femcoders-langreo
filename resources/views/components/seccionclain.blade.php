59a86e91b7ce89cddb1310e601d9b5ad5bd5c76b
Paz Manrique <nautilus1985@gmail.com>
2022-10-05 10:43:31 +0200

    playing with video options

--- resources/views/components/seccionclain.blade.php
+++ resources/views/components/seccionclain.blade.php
@@ -1,25 +1,27 @@
 <section class="conten-clain">
 
   <div class="img-principal" >
-    <img class="girl" src="{{ asset('img/foto3.jpeg') }}" alt=""> 
+    <video src="{{ asset('img/f5.mp4') }}" autoplay="true" muted="false" loop="true"></video>
+    {{-- <img class="girl" src="{{ asset('img/foto3.jpeg') }}" alt="">  --}}
     </div> 
     
     <div class="text-clain">
     <p class="txt-clain1">
         <strong>
-    ¿Conoces a Ada Lovelace o Grae Hopper? Son sólo dos ejemplo de las numerosas mujeres que revolucionaron el mundo de la tecnología. 
+    ¿Conoces a Ada Lovelace o Grae Hopper? Son sólo dos ejemplo de las numerosas mujeres que revolucionaron el mundo de la tecnología.<br><br> 
+    Tú también puedes apender a crear tu propia app, juegos o páginas web.
+        ¡Es mucho más facil y divertido de lo que te imaginas!<br><br>
+        Atrévete a descubrir cómo funciona el código, y comprueba por ti misma que la tecnología ¡mola! 
     </strong></p>
 
     <p class="text-clain2">
         <strong>
-            Es hora de volver a cambiar la historia y romper los estereotipos, porque... ¡Todas podemos aprender a programar¡</strong></p>
+            Es hora de volver a cambiar la historia y romper los estereotipos, porque... ¡Todas podemos aprender a programar!</strong></p>
     
         <p class="txt-clain3">
             <strong>
                 
-        Tú también puedes apender a crear tu propia app, juegos o páginas web.
-        ¡Es mucho más facil y divertido de lo que te imaginas!<br>
-        Atrévete a descubrir cómo funciona el código, y comprueba por ti misma que la tecnología ¡mola! 
+        
         </strong></p>
     </div>
     
