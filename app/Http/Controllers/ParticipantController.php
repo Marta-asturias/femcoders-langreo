5868ae5000163f78a75de2a3b6f4e81067a10c6e
Sara <sarap120298@gmail.com>
2022-10-07 13:56:05 +0200

    form changes added

--- app/Http/Controllers/ParticipantController.php
+++ app/Http/Controllers/ParticipantController.php
@@ -4,21 +4,25 @@ namespace App\Http\Controllers;
 
 use App\Models\Participant;
 use App\Repositories\Participant\ParticipantRepository;
+use App\Repositories\Workshop\WorkshopRepository;
 use Illuminate\Http\Request;
 
 
 
 class ParticipantController  {
 
-private ParticipantRepository $repository;
+private ParticipantRepository $participantRepository;
+private WorkshopRepository $workshopRepository;
 
     public function __construct() {
-    $this->repository= new ParticipantRepository(); 
+    $this->participantRepository= new ParticipantRepository();
+    $this->workshopRepository= new WorkshopRepository();
     }
 
     public function index(){
-    $participant = $this->repository->getall();
-    return view('participant')->with('participant', $participant);
+    $participant = $this->participantRepository->getall();
+    $workshop = $this->workshopRepository->getAll();
+    return view('participant')->with(['participant' => $participant, 'workshop' => $workshop]);
 
     }
 
@@ -33,7 +37,7 @@ private ParticipantRepository $repository;
         if (isset($_POST['sendForm'])) {
             if (isset($_POST['legals']) && $_POST['legals'] == '1')
                 echo '<div class="alert alert-success">Has aceptado correctamente las condiciones de uso.</div>';
-                $this->repository->saveParticipant($request);
+                $this->participantRepository->saveParticipant($request);
                 return $this->index();
  
 }
--- app/Repositories/Workshop/WorkshopRepository.php
+++ app/Repositories/Workshop/WorkshopRepository.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use RealRashid\SweetAlert\Facades\Alert;
 
+
 class WorkshopRepository  {
 private Workshop $workshop;
 
@@ -26,7 +27,11 @@ private Workshop $workshop;
             'details' => 'required',
             'age'  => 'required',
             'duration'  => 'required',
-            'format'  => 'required'
+            'format'  => 'required',
+            'date1' => 'required',
+            'date2' => 'required',
+            'date3' => 'required',
+            'date4' => 'required'
         ]);
         $fileName = time().'.'.$request->file->extension();
         $request->file->move(public_path('storage'), $fileName);
@@ -37,6 +42,10 @@ private Workshop $workshop;
         $this->workshop->age = $request->get('age');
         $this->workshop->duration = $request->get('duration');
         $this->workshop->format = $request->get('format');
+        $this->workshop->date1 = $request->get('date1');
+        $this->workshop->date2 = $request->get('date2');
+        $this->workshop->date3 = $request->get('date3');
+        $this->workshop->date4 = $request->get('date4');
         
         return $this->workshop->save();  
  
@@ -82,7 +91,7 @@ private Workshop $workshop;
     public function destroyWorkshop(Request $request)
     {
         $workshop = workshop::find($request->id); 
-        Alert::success('Success', 'Has eliminado el workshop');       
+        // Alert::success('Success', 'Has eliminado el workshop');       
         return $workshop->delete();
     
     }
