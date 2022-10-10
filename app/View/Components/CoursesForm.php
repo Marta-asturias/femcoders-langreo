7287ca9ad5f0ef0420322ad794cdbd825af51af7
Paz Manrique <nautilus1985@gmail.com>
2022-10-10 10:01:50 +0200

    adding public constructor to the component coursesform

--- app/View/Components/CoursesForm.php
+++ app/View/Components/CoursesForm.php
@@ -6,15 +6,18 @@ use Illuminate\View\Component;
 
 class CoursesForm extends Component
 {
+    public $workshops;
     /**
      * Create a new component instance.
      *
      * @return void
      */
-    public function __construct()
-    {
-        //
-    }
+   
+        public function __construct($workshops)
+        {
+            $this->workshops=$workshops;
+      }
+    
 
     /**
      * Get the view / contents that represent the component.
