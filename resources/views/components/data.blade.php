f99a6eed1148e8b7df1ee0d60695f58c7e821921
Juliet Linarez <julietlinarez@gmail.com>
2022-10-13 13:24:05 +0200

    style in componet data

--- resources/views/components/data.blade.php
+++ resources/views/components/data.blade.php
@@ -1,23 +1,23 @@
-<div>
+<div class="conten-data">
     @foreach ($workshop as $workshops)
-    <form class="description-form" action="{{ route('getdate', $workshops) }}" method="GET" enctype="multipart/form-data">
+    <form class="form-data" action="{{ route('getdate', $workshops) }}" method="GET" enctype="multipart/form-data">
         @csrf
         @endforeach
-        <div class="text-des-edit">
-            <x-label for="price" :value="__('')" />
-            <span>Fecha 1:</span><x-input id="date1" class="mt-1 w-24" type="text" name="date1" :value="old('date1') ?? $workshop->date1" required />
+        <div class="text-data">
+            <x-label class="label-data" for="price" :value="__('Fecha 1:')" />
+            <x-input id="date1" class="mt-1 w-24" type="text" name="date1" :value="old('date1') ?? $workshop->date1" required />
         </div>
-        <div class="text-des-edit">
-            <x-label for="price" :value="__('')" />
-            <span>Fecha 2:</span><x-input id="date1" class="mt-1 w-24" type="text" name="date2" :value="old('date2') ?? $workshop->date2" required />
+        <div class="text-data">
+            <x-label class="label-data"  for="price" :value="__('Fecha 2:')" />
+            <x-input id="date1" class="mt-1 w-24" type="text" name="date2" :value="old('date2') ?? $workshop->date2" required />
         </div>
-        <div class="text-des-edit">
-            <x-label for="price" :value="__('')" />
-            <span>Fecha 3:</span><x-input id="date1" class="mt-1 w-24" type="text" name="date3" :value="old('date3') ?? $workshop->date3" required />
+        <div class="text-data">
+            <x-label class="label-data"  for="price" :value="__('Fecha 3:')" />
+            <x-input id="date1" class="mt-1 w-24" type="text" name="date3" :value="old('date3') ?? $workshop->date3" required />
         </div>
-        <div class="text-des-edit">
-            <x-label for="price" :value="__('')" />
-            <span>Fecha 4:</span><x-input id="date1" class="mt-1 w-24" type="text" name="date4" :value="old('date4') ?? $workshop->date4" required />
+        <div class="text-data">
+            <x-label class="label-data"  for="price" :value="__('Fecha 4:')" />
+            <x-input id="date1" class="mt-1 w-24" type="text" name="date4" :value="old('date4') ?? $workshop->date4" required />
         </div>
         
     </form>
