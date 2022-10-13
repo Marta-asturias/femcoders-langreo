df40494ce50d3ed931c2c0ba86b5ab34f34bc513
Marta-asturias <marta.gonzalez@domoticadavinci.com>
2022-10-13 11:56:58 +0200

    igual

--- resources/views/components/data.blade.php
+++ resources/views/components/data.blade.php
@@ -1,9 +1,13 @@
 <div>
-    <form class="description-form" action="{{ route('getdate') }}" method="GET" enctype="multipart/form-data">
+    @foreach ($workshop as $workshops)
+    <form class="description-form" action="{{ route('getdate', $workshops) }}" method="GET" enctype="multipart/form-data">
         @csrf
+        @endforeach
         <div class="form-line">
             <x-label class="label-form" for="date1" :value="__('Fecha')" />
             <x-input id="date1" class="input-form" type="text" name="date1" :value="old('date1')" required autofocus />
         </div>
+        
     </form>
+  
 </div>
