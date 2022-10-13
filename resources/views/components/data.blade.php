cfdf66e8c9e5782dd8c2b1d10001c46b5ea8f63f
Marta-asturias <marta.gonzalez@domoticadavinci.com>
2022-10-13 12:09:31 +0200

    recuperacion de fecha

--- resources/views/components/data.blade.php
+++ resources/views/components/data.blade.php
@@ -3,9 +3,9 @@
     <form class="description-form" action="{{ route('getdate', $workshops) }}" method="GET" enctype="multipart/form-data">
         @csrf
         @endforeach
-        <div class="form-line">
-            <x-label class="label-form" for="date1" :value="__('Fecha')" />
-            <x-input id="date1" class="input-form" type="text" name="date1" :value="old('date1')" required autofocus />
+        <div class="text-des-edit">
+            <x-label for="price" :value="__('')" />
+            <span>Fecha 1:</span><x-input id="date1" class="mt-1 w-24" type="text" name="date1" :value="old('date1') ?? $workshop->date1" required />
         </div>
         
     </form>
