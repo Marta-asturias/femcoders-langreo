be6fec28b1cb3942c61b89de3e5d978fe6d9a1c4
Sara <sarap120298@gmail.com>
2022-10-20 13:43:43 +0200

    adding changes on branch sara

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -3,15 +3,23 @@
 namespace App\Http\Controllers;
 
 use App\Mail\tryEmail;
+use App\Repositories\Participant\ParticipantRepository;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 
 class MailController extends Controller
 {
-    public function tryEmail() {
+    private ParticipantRepository $participantRepository;
+
+    public function __construct(){
+        $this->repository = new ParticipantRepository;
+    }
+        
+    public function tryEmail(Request $request) {
+        $participant = $this->participantRepository->getByemail($request);
         $mail = new tryEmail;
 
-        Mail::to('Sarap120298@gmail.com')->send($mail);
+        Mail::to($participant)->send($mail);
         return 'Mensaje Enviado';
 
     }
