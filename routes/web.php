f59403e748514ba4e111a4f7a514463bad37f8d7
Marta-asturias <marta.gonzalez@domoticadavinci.com>
2022-10-20 15:16:50 +0200

    añadir ruta

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,23 @@
 <?php
 
-use App\Http\Controllers\MailController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\AdminWelcomeController;
+use App\Http\Controllers\DeleteUserController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MiniGamesController;
+use App\Http\Controllers\ParticipantController;
+use App\Http\Controllers\ParticipantViewController;
+use App\Http\Controllers\ResourcesController;
+use App\Http\Controllers\ShowUserController;
+use App\Http\Controllers\UpdateUSerController;
+use App\Http\Controllers\UserMiniGamesController;
+use App\Http\Controllers\UserResourcesController;
+use App\Http\Controllers\UserWorkshopsController;
+use App\Http\Controllers\WorkshopController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Repositories\Participant\ParticipantRepository;
 use Illuminate\Support\Facades\Route;
-use App\Mail\InscriptionMail;
-use Illuminate\Support\Facades\Mail;
+use App\Http\Controllers\MailController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,9 +29,16 @@ use Illuminate\Support\Facades\Mail;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/mail', [MailController::class, 'tryEmail']);
+Route::get('/{id}/participant', [ParticipantController::class, 'createParticipant' ])->name("createParticipant");
+Route::post('/{id}/participant', [ParticipantController::class, 'save' ])->name("inscription");
+Route::get('/workshops', [UserWorkshopsController::class, 'getWorkshops' ])->name("getWorkshops");
+Route::get('/minigames', [UserMiniGamesController::class, 'getMiniGames' ])->name("minigames");
+Route::get('/resources', [UserResourcesController::class, 'getResources' ])->name("resources");
 
+Route::get('/welcome', function () {
+    return view('welcome');
+});
 
 Route::get('/welcome', [HomeController::class, 'welcome' ])->name("welcome");
 
@@ -66,8 +87,7 @@ Route::put('/admin/users/{id}/edit', [UpdateUSerController ::class, 'update' ])-
 });
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth','verified'])->name('dashboard');
+Route::get('/register', [RegisteredUserController::class, 'create'])->middleware(['auth.basic'])->name('register');
 
 require __DIR__.'/auth.php';
-
-// Email related routes
 Route::get('mail/send', 'MailController@send');
