14a0082d900f0022f45ec61c0afc274f81e38a18
Paz Manrique <nautilus1985@gmail.com>
2022-10-20 12:08:45 +0200

    adding log out

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -19,6 +19,7 @@ class RouteServiceProvider extends ServiceProvider
      * 
      */
     public const HOME = '/dashboard';
+    public const LOGIN = '/login';
     
     /* if (Request::url() == route('login')) {
         HOME = '/dashboard';
