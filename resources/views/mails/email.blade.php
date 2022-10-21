272910c378864a6cea546ec0154c30bb8db8355c
Juliet Linarez <julietlinarez@gmail.com>
2022-10-21 19:38:08 +0200

    workign in mail

--- resources/views/mails/email.blade.php
+++ resources/views/mails/email.blade.php
@@ -1,21 +1,31 @@
-
-    <div class="img-header">
-        <img id="imageEmail" src="{{ asset('img/logofempng.png') }}" />
+<x-head/>
+<div class="conten-mail">
+    <div class="img-mail">
+        <img src="{{ asset('img/logofempng.png') }}" class="imageEmail"alt="...">
+        <img src="{{ asset('img/factoria-web.png') }}" class="imageEmail"alt="...">
     </div>
-<h1>Hi, Hello, Hola, Holi!</h1>
 
-<p>
+    <div class="greeting-mail">
+        <h1 class="txt-greeting">Hi, Hello, Hola, Holi!</h1>
+    </div>
 
-    Hemos recibido tu inscripción <strong>{{$data['first_name']}}</strong> <strong>{{$data['last_name']}}</strong><br>
-    <strong> esperamos online el <strong>{{$data['date']}}</strong> para el taller Conoce Python!</strong> Recuerda que es imprescindible que tengas micrófono, cámara y ordenador para poder disfrutar del taller. 
-
-
-</p>
-
-<p>
-    ¡Programar mola! 
-
-Equipo Talleres Fem Coders F5
-talleres@factoriaf5.org
-</p>
-
+    <div class="txt-mail">
+        <p class="data-mail">
+            Hemos recibido tu inscripción <strong>{{$data['first_name']}}</strong> <strong>{{$data['last_name']}}</strong>
+        </p>
+        <p class="data-mail">
+            <strong>Te esperamos online el <strong>{{$data['date']}}</strong> para el taller Conoce Python!</strong> Recuerda que es imprescindible que tengas micrófono, cámara y ordenador para poder disfrutar del taller. 
+        </p>
+    </div>
+    <div class="firms-mail">
+        <p class="txt-firms">
+            ¡Programar mola! 
+        </p>
+        <p class="txt-firms">
+            Equipo Talleres Fem Coders F5
+        </p>
+        <p class="txt-firms">
+            talleres@factoriaf5.org
+        </p>
+    </div>
+</div>
